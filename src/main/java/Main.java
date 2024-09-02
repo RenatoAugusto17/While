b2ca320f2483b8c35624f8268242f4aacb1d08d6
RenatoAugusto17 <169068426+RenatoAugusto17@users.noreply.github.com>
2024-09-02 12:17:30 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+  public static void main(String[] args) {
+ int contador = 1;
+ String nome = "Renato";
+
+    while(contador < 11) {
+      System.out.println("Bom dia!");
+      contador = contador + 1;
+    }
+System.out.println(nome);
+    
+  }
+}
